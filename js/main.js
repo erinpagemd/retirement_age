28c7ca578842095587be93169159fb18c5d63d0d
YOUR NAME <erinpagemd@yahoo.com>
2014-11-25 01:26:10 -0600

    age at retirement

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 $(document).ready(initialize);
 
   function initialize(){
-    //stuff here
+
+    var date = [];
+    $('#submit').click(function(){
+      var bday = $('#bday').val();
+      date = bday.split('-');
+      var year = date[0];
+      console.log(bday);
+      console.log(date);
+      console.log(year);
+
+      if(year <= 1937){
+        alert('your retirement age is 65 years and 0 months');
+      }else if(year == 1938){
+        alert('your retirement age is 65 years and 2 months');
+      }else if(year == 1939){
+        alert('your retirement age is 65 years and 4 months');
+      }else if(year == 1940){
+        alert('your retirement age is 65 years and 6 months');
+      }else if(year == 1941){
+        alert('your retirement age is 65 years and 8 months');
+      }else if(year == 1942){
+        alert('your retirement age is 65 years and 10 months');
+      }else if(year >= 1943 && year <= 1954){
+        alert('your retirement age is 66 years and 0 months');
+      }else if(year == 1955){
+        alert('your retirement age is 66 years and 2 months');
+      }else if(year == 1956){
+        alert('your retirement age is 66 years and 4 months');
+      }else if(year == 1957){
+        alert('your retirement age is 66 years and 6 months');
+      }else if(year == 1958){
+        alert('your retirement age is 66 years and 8 months');
+      }else if(year == 1959){
+        alert('your retirement age is 66 years and 10 months');
+      }else if(year >= 1960){
+        alert('your retirement age is 67 years and 0 months');
+      }else{
+        alert('something is happening');
+      }
+    });
+
   }
